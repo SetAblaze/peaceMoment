a5fc5cea4a6cf291d97806adcf3fb5c0e06c9354
SetAblaze <88194169+SetAblaze@users.noreply.github.com>
2023-04-02 17:52:16 +0300

    Updates

---
+++ precache-manifest.ecef2448730ca4e1a1f09e53d34e288a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "224201bdcad44306c67e",
+    "url": "/peaceMoment/static/css/main.1725294c.chunk.css"
+  },
+  {
+    "revision": "224201bdcad44306c67e",
+    "url": "/peaceMoment/static/js/main.50da8b00.chunk.js"
+  },
+  {
+    "revision": "2b7de9506d2d5f900083",
+    "url": "/peaceMoment/static/js/runtime~main.4cd9badd.js"
+  },
+  {
+    "revision": "5004e11e068a8d230f63",
+    "url": "/peaceMoment/static/css/2.927f5086.chunk.css"
+  },
+  {
+    "revision": "5004e11e068a8d230f63",
+    "url": "/peaceMoment/static/js/2.dfbf29c9.chunk.js"
+  },
+  {
+    "revision": "10dee9060cf37b714257f74e3a1c7115",
+    "url": "/peaceMoment/index.html"
+  }
+];
